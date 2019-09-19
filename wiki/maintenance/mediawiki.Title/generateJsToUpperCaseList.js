ce51c5ba942ab18759fb50ff45ce58aded23fe28
DESKTOP-T5T7LS5\Zhi Kuan <xiaoichiban@hotmail.com>
2019-09-19 17:07:14 +0800

    remove wiki

--- wiki/maintenance/mediawiki.Title/generateJsToUpperCaseList.js
+++
@@ -1,8 +0,0 @@
-/* eslint-env node, es6 */
-var i, chars = [];
-
-for ( i = 0; i < 65536; i++ ) {
-	chars.push( String.fromCharCode( i ).toUpperCase() );
-}
-// eslint-disable-next-line no-console
-console.log( JSON.stringify( chars ) );
