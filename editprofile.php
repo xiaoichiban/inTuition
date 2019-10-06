5a066aae093f61e661bd6115fc0c87e3c69c63c3
apollo717 <54750246+apollo717@users.noreply.github.com>
2019-10-06 15:13:24 +0800

    Update editprofile.php

--- editprofile.php
+++ editprofile.php
@@ -7,17 +7,12 @@ $sql = "SELECT name, email FROM account where username = '$login_session';";
 $result = mysqli_query($db, $sql);
 $count = mysqli_num_rows($result);
 
-
-
 if ($count > 0) {
     echo "<br>";
 	echo "<br>";
 	echo "<h2> My Details </h2>";
     // output data of each row
     
-	
-	
-	
 	$row = mysqli_fetch_assoc($result);
         echo 
 	   '
@@ -27,25 +22,18 @@ if ($count > 0) {
 		<td><input type="text" name="name" value=' . $row['name']. '></td></tr>
 		<tr><td>Email:</td>
 		<td><input type="text" name="email" value=' . $row['email']. '></td></tr>
-		
-	
 		<tr>
 		<td>
 		 <input type="submit" name="submit" value="Save Changes"/>
 		 </td>
 		 </tr>
-	
 		</form>
 	   </table>
-	   
 	   ';
 
 	echo "<br>";
     echo "<br> <a href='welcome.php'> BACK </a>";
     echo "<br>";
-	   
-	   
-	   
     
 } else {
     echo "0 results";
