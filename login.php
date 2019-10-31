892a2095bfd4b11c30956259105fb8d90379dce9
apollo717 <54750246+apollo717@users.noreply.github.com>
2019-10-31 23:21:32 +0800

    fixed login
    fixed login to centrailized login

--- login.php
+++ login.php
@@ -5,19 +5,37 @@
      
    if($_SERVER["REQUEST_METHOD"] == "POST") {
       // username and password sent from form 
+	  
+	  
+	  if(! isset($_POST['username']) || ! isset($_POST['password'])){
+		echo "<h3 align='center'><font color='red'>LOGIN FAILED</font></h3>";
+		echo "<h3 align='center'>Login Failed >> Your Account has been Deactivated or Banned</h3>";
+		echo "<h3 align='center'><a href='login.html' > BACK </a></h3>";
+		return;
+	  }
+	  
       
       $myusername = mysqli_escape_string($db,$_POST['username']);
       $mypassword = mysqli_escape_string($db,$_POST['password']); 
 	  
 
-      $sql = "SELECT username,status FROM account WHERE username = '$myusername' and password = '$mypassword';";
+      $sql = "SELECT username,status,account_type 
+	  FROM account WHERE username = '$myusername' and password = '$mypassword';";
 
       $result = mysqli_query($db,$sql);
       $row = mysqli_fetch_assoc($result);
       $count = mysqli_num_rows($result);
-      
 	  $status = $row['status'];
+	  $account_type = $row['account_type'];
 
+	  // If result matched $myusername and $mypassword, table row must be 1 row
+	  // CASE : BANNED OR DEACTIVATED
+      if($count != 1) {
+		echo "<h3 align='center'><font color='red'>LOGIN FAILED</font></h3>";
+		echo "<h3 align='center'><a href='login.html' > BACK </a></h3>";
+		return;
+      }
+	  
 	  
 	  
 	  // If result matched $myusername and $mypassword, table row must be 1 row
@@ -26,7 +44,7 @@
 		echo "<h3 align='center'><font color='red'>LOGIN FAILED</font></h3>";
 		echo "<h3 align='center'>Login Failed >> Your Account has been Deactivated or Banned</h3>";
 		echo "<h3 align='center'><a href='login.html' > BACK </a></h3>";
-		
+		return;
       }
 	  
 	  
@@ -39,15 +57,40 @@
 		 $resulttime = mysqli_query($db, $sqlgettime);
 		 $lastloginrow = mysqli_fetch_row($resulttime);
 		 $last = $lastloginrow[0];
+		 
+		 
 		 $_SESSION['login_user'] = $myusername;
+		 $_SESSION['username'] = $myusername;
 		 $_SESSION['last_login'] = $last;
-		 //echo $_SESSION['login_user'];
-		 //echo $_SESSION['last_login'];
+		 $_SESSION['account_type'] = $account_type;
+		 
 		 date_default_timezone_set("Singapore");
 		 $timenow = date("Y-m-d");
 		 $sqltime = "UPDATE account set last_login='$timenow' WHERE username = '$myusername';";
 		 $timeupdatevar = mysqli_query($db,$sqltime);
-         header("location: welcome.php");
+		 
+		 
+		 echo $_SESSION['login_user']."  ";
+		 echo $_SESSION['last_login']."  ";
+		 echo $_SESSION['account_type']."  ";
+		 
+		 if ($account_type == "student"){
+			header("location: ./student/welcome.php");
+		 }
+		 else if ($account_type == "tc"){
+			header("location: ./tc/welcome.php");
+		 }
+		 else if ($account_type == "tutor") {
+			header("location: ./tutor/welcome.php");
+		 }
+		 else {
+			 
+			 
+			//header("location: welcome.php");
+			 
+		 }
+		 
+		 
       }
 	  
 	  
---
+++ student/index.php
@@ -0,0 +1,1 @@
+<h3>STUDENT HOME PAGE</h3>
