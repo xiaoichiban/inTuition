828c48f52578ebdd09d7cb3a49835f9ea6739883
RouJie <e0196675@u.nus.edu>
2019-11-02 21:08:25 +0800

    update search modules and fixed bug

--- student/searchmodules.php
+++ student/searchmodules.php
@@ -26,18 +26,20 @@ include 'session.php';
       "<tr><th>module_name</th>" .
       "<th>offered by</th>";
       while ($row = mysqli_fetch_row($result)) {
-        echo "<tr><th>". $row[1]."</th>
+        $module_name = $row[1];
+        echo "<tr><th>". $module_name."</th>
         <th>". $row[6]."</th>";
-        $sql_check = "SELECT * FROM module WHERE name IN (SELECT m.name FROM module m, enroll e WHERE m.id = e.mod_id AND e.student = '$username');";
+        $sql_check = "SELECT * FROM module WHERE name = '$module_name' AND name IN (SELECT name FROM module WHERE id IN (SELECT mod_id FROM enroll WHERE student = '$username'));";
         $result_check = mysqli_query($db, $sql_check);
         if (mysqli_num_rows($result_check)==0){
-          echo"<th><a href = 'registermodule.php?module_name=".$row[1]."'>Register</a></th>";
+          echo"<th><a href = 'registermodule.php?module_name=$module_name'>Register</a></th>";
         }
         else{
-          $sql3 = "SELECT * FROM module WHERE id IN (SELECT m.id FROM module m, enroll e WHERE m.id = e.mod_id AND m.name = '$row[1]' AND e.student = '$username');";
+          $sql3 = "SELECT * FROM module WHERE id IN (SELECT id FROM module WHERE name = '$module_name' AND id IN (SELECT mod_id FROM enroll WHERE student = '$username'));";
           $result3 = mysqli_query($db, $sql3);
-          $row3 = mysqli_fetch_row($result3);
-          echo"<th><a href = 'viewmodule.php?module_id=".$row3[0]."'>View</a></th>";
+          while($row3 = mysqli_fetch_row($result3)){
+            echo"<th><a href = 'viewmodule.php?module_id=".$row3[0]."'>View</a></th>";
+          }
         }
         echo"</tr>";
       }
@@ -56,18 +58,20 @@ include 'session.php';
         "<tr><th>module_name</th>" .
         "<th>offered by</th>";
         while ($row = mysqli_fetch_row($result2)) {
-          echo "<tr><th>". $row[1]."</th>
+          $module_name = $row[1];
+          echo "<tr><th>". $module_name."</th>
           <th>". $row[6]."</th>";
-          $sql_check = "SELECT * FROM module WHERE name IN (SELECT m.name FROM module m, enroll e WHERE m.id = e.mod_id AND e.student = '$username');";
+          $sql_check = "SELECT * FROM module WHERE name = '$module_name' AND name IN (SELECT name FROM module WHERE id IN (SELECT mod_id FROM enroll WHERE student = '$username'));";
           $result_check = mysqli_query($db, $sql_check);
           if (mysqli_num_rows($result_check)==0){
-            echo"<th><a href = 'registermodule.php?module_name=".$row[1]."'>Register</a></th>";
+            echo"<th><a href = 'registermodule.php?module_name=$module_name'>Register</a></th>";
           }
           else{
-            $sql3 = "SELECT * FROM module WHERE id IN (SELECT m.id FROM module m, enroll e WHERE m.id = e.mod_id AND m.name = '$row[1]' AND e.student = '$username');";
+            $sql3 = "SELECT * FROM module WHERE id IN (SELECT id FROM module WHERE name = '$module_name' AND id IN (SELECT mod_id FROM enroll WHERE student = '$username'));";
             $result3 = mysqli_query($db, $sql3);
-            $row3 = mysqli_fetch_row($result3);
-            echo"<th><a href = 'viewmodule.php?module_id=".$row3[0]."'>View</a></th>";
+            while($row3 = mysqli_fetch_row($result3)){
+              echo"<th><a href = 'viewmodule.php?module_id=".$row3[0]."'>View</a></th>";
+            }
           }
           echo"</tr>";
         }
