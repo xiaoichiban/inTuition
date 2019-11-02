25d26bdb78c04b7a0007720a9ce2e12fd38325e8
apollo717 <54750246+apollo717@users.noreply.github.com>
2019-11-02 21:22:53 +0800

    Update login.php
    fixed Login

--- login.php
+++ login.php
@@ -31,67 +31,64 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
 		echo "   ".$_POST["password"];
 		echo "   ".$row["password"];
 		
-			// Password correct
-			if(password_verify($_POST["password"], $row["password"]) == true)
-			{	
+	// Password correct
+	if(password_verify($_POST["password"], $row["password"]) == true)
+	{	
+
+
+		$status = $row['status'];
+		$account_type = $row['account_type'];
+		$username_var = $row['username'];
 		
 		
-				$status = $row['status'];
-				$account_type = $row['account_type'];
-				$username_var = $row['username'];
-				
-				
-				
-				// If result matched $myusername and $mypassword, table row must be 1 row
-				// CASE : BANNED OR DEACTIVATED
-				  if($status!='active') {
-					echo "<h3 align='center'><font color='red'>LOGIN FAILED</font></h3>";
-					echo "<h3 align='center'>Login Failed >> Your Account has been Deactivated or Banned</h3>";
-					echo "<h3 align='center'><a href='login.html' > BACK </a></h3>";
-					return;
-				  }
-				
-				
-				
-				
-				
-				
+		
+		// If result matched $myusername and $mypassword, table row must be 1 row
+		// CASE : BANNED OR DEACTIVATED
+		  if($status!='active') {
+			echo "<h3 align='center'><font color='red'>LOGIN FAILED</font></h3>";
+			echo "<h3 align='center'>Login Failed >> Your Account has been Deactivated or Banned</h3>";
+			echo "<h3 align='center'><a href='login.html' > BACK </a></h3>";
+			return;
+		  }
 		
 		
-				$_SESSION['user_id'] = $row['user_id'];
-				$_SESSION['username'] = $username_var;
-				$_SESSION['login_user'] = $username_var;
+
+		$_SESSION['user_id'] = $row['user_id'];
+		$_SESSION['username'] = $username_var;
+		$_SESSION['login_user'] = $username_var;
+		
+			
+		
+		
+		// Setting all session variables
+		// 
+		$sub_query = "UPDATE account SET last_seen = now() WHERE username = '$username_var' ;";
+		$result = mysqli_query($db, $sub_query);
+		
+		
+		//$statement = $db->prepare($sub_query);
+		//$statement->execute();
+		//$_SESSION['login_details_id'] = $db->lastInsertId();
+
+		//header('location:index.php');
+		
+		
+		
+				 
+		 if ($account_type == "student"){
+			header("location: ./student/studentdashboard.php");
+		 }
+		 else if ($account_type == "tc"){
+			header("location: ./tc/tcdashboard.php");
+		 }
+		 else if ($account_type == "tutor") {
+			header("location: ./tutor/tutordashboard.php");
+		 }
+		 else {
+			header("location: loginFailed.php");
+		 }
 				
-					
-				
-				
-				// Setting all session variables
-				// 
-				$sub_query = "UPDATE account SET last_seen = now() WHERE username = '".$row['username']."' ";
-				$statement = $connect->prepare($sub_query);
-				$statement->execute();
-				$_SESSION['login_details_id'] = $connect->lastInsertId();
-				
-				
-				//header('location:index.php');
-				
-				
-				
-						 
-				 if ($account_type == "student"){
-					header("location: ./student/studentdashboard.php");
-				 }
-				 else if ($account_type == "tc"){
-					header("location: ./tc/tcdashboard.php");
-				 }
-				 else if ($account_type == "tutor") {
-					header("location: ./tutor/tutordashboard.php");
-				 }
-				 else {
-					header("location: loginFailed.php");
-				 }
-						
-				
+		
 				
 				
 				
