fbd1fcc5175ab6acdde4663389ca4b092cebf1c2
yinggs <yinghuiseah@u.nus.edu>
2019-11-09 16:40:46 +0800

    integrated the chameleon templete in

---
+++ student/layout/sidebar.php
@@ -0,0 +1,88 @@
+<html>
+
+<body>
+<?php 
+  $activePage = basename($_SERVER['PHP_SELF'], ".php");
+?>
+
+<nav class="header-navbar navbar-expand-md navbar navbar-without-dd-arrow fixed-top">
+  <div class="navbar-wrapper">
+        <div class="navbar-container content">
+          <div class="collapse navbar-collapse show" id="navbar-mobile">
+            <ul class="nav navbar-nav mr-auto float-left">
+              <li class="nav-item d-block d-md-none"><a class="nav-link nav-menu-main menu-toggle hidden-xs" href="#">
+                <i class="ft-menu"></i></a></li>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+</nav>
+
+<div class="main-menu menu-fixed menu-light menu-accordion menu-shadow " data-scroll-to-active="true" data-img="../theme-assets/images/backgrounds/02.jpg">
+      <div class="navbar-header">
+        <ul class="nav navbar-nav flex-row">       
+          <li class="nav-item mr-auto"><a class="navbar-brand" href="index.html"><img class="brand-logo" alt="Chameleon admin logo" src="./layout/theme-assets/images/logo/intuition_logo.png"/>
+              <h3 class="brand-text">inTuition</h3></a></li>
+          <li class="nav-item d-md-none"><a class="nav-link close-navbar"><i class="ft-x"></i></a></li>
+        </ul>
+      </div>
+      <div class="main-menu-content">
+        <ul class="navigation navigation-main" id="main-menu-navigation" data-menu="menu-navigation">
+          <li class="<?= ($activePage == 'studentdashboard') ? 'active':'nav-item'; ?>"><a href="studentdashboard.php"><i class="ft-home"></i><span class="menu-title" data-i18n="">Dashboard</span></a>
+          </li>
+          <li class="<?= ($activePage == 'searchmodules') ? 'active':'nav-item'; ?>"><a href="searchmodules.php"><i class="ft-home"></i><span class="menu-title" data-i18n="">Search modules</span></a>
+          </li>
+          <li class="<?= ($activePage == 'index') ? 'active':'nav-item'; ?>"><a href="viewstudentmodules.php"><i class="ft-pie-chart"></i><span class="menu-title" data-i18n="">My Modules</span></a>
+          </li>
+          <li class="<?= ($activePage == 'index') ? 'active':'nav-item'; ?>"><a href=""><i class="ft-droplet"></i><span class="menu-title" data-i18n="">Chat</span></a>
+          </li>
+          <li class="<?= ($activePage == 'index') ? 'active':'nav-item'; ?>"><a href=""><i class="ft-layers"></i><span class="menu-title" data-i18n="">My Profile</span></a>
+          </li>
+          <li class="<?= ($activePage == 'complain') ? 'active':'nav-item'; ?>"><a href="complain.php"><i class="ft-box"></i><span class="menu-title" data-i18n="">Feedback</span></a>
+          </li>
+          <li class="<?= ($activePage == 'viewtimetable') ? 'active':'nav-item'; ?>"><a href="viewtimetable.php"><i class="ft-bold"></i><span class="menu-title" data-i18n="">My Timetable</span></a>
+          </li>
+          <li class="nav-item"><a href="logout.php"><i class="ft-credit-card"></i><span class="menu-title" data-i18n="">Logout</span></a>
+          </li>
+          <li class="nav-item pl-2">
+            <?php
+              echo "Logged in As: ";
+              $user_id=$_SESSION['user_id'];
+              $username=$_SESSION['username'];
+              echo "$username<br />";
+              $login_user=$_SESSION['login_user'];
+              $sql = "SELECT * FROM notification WHERE receiver = '$username' AND isRead = '0';";
+              $result = mysqli_query($db, $sql);
+              if (mysqli_num_rows($result) > 0){
+                $sql1 = "SELECT COUNT(*) FROM notification WHERE receiver='$username' and isRead = '0' GROUP BY receiver;";
+                $result1 = mysqli_query($db,$sql1);
+                $row1 = mysqli_fetch_row($result1);
+                echo "Notifications(".$row1[0].")";
+                while ($row = mysqli_fetch_row($result)){
+                  echo "<h5>" . $row[1] . "</h5>";
+                  $tempstore[] = $row[0];
+                }
+                foreach ($tempstore as $value) {
+                  $sql2 = "UPDATE notification SET isRead = '1' WHERE id = '$value' AND receiver = '$username'";
+                  $result2 = mysqli_query($db, $sql2);
+                  if (!$result2){
+                    echo "unsuccessful";
+                  }
+                }
+              }
+              else{
+                echo "Notifications";
+              }
+            ?>
+
+          </li>
+        </ul>
+
+      </div>
+      <div class="navigation-background"></div>
+    </div>
+
+
+</body>
+</html>
--- student/studentdashboard.php
+++ student/studentdashboard.php
@@ -1,56 +1,101 @@
 <html>
   <head>
     <title>Dashboard</title>
+    <link rel="apple-touch-icon" href="./layout/theme-assets/images/ico/apple-icon-120.png">
+    <link rel="shortcut icon" type="image/x-icon" href="./layout/theme-assets/images/ico/favicon.ico">
+    <link href="https://fonts.googleapis.com/css?family=Muli:300,300i,400,400i,600,600i,700,700i%7CComfortaa:300,400,700" rel="stylesheet">
+    <link href="https://maxcdn.icons8.com/fonts/line-awesome/1.1/css/line-awesome.min.css" rel="stylesheet">
+    <!-- BEGIN VENDOR CSS-->
+    <link rel="stylesheet" type="text/css" href="./layout/theme-assets/css/vendors.css">
+    <link rel="stylesheet" type="text/css" href="./layout/theme-assets/vendors/css/charts/chartist.css">
+    <!-- END VENDOR CSS-->
+    <!-- BEGIN CHAMELEON  CSS-->
+    <link rel="stylesheet" type="text/css" href="./layout/theme-assets/css/app-lite.css">
+    <!-- END CHAMELEON  CSS-->
+    <!-- BEGIN Page Level CSS-->
+    <link rel="stylesheet" type="text/css" href="./layout/theme-assets/css/core/menu/menu-types/vertical-menu.css">
+    <link rel="stylesheet" type="text/css" href="./layout/theme-assets/css/core/colors/palette-gradient.css">
+    <link rel="stylesheet" type="text/css" href="./layout/theme-assets/css/pages/dashboard-ecommerce.css">
+    <!-- END Page Level CSS-->
+    <!-- BEGIN Custom CSS-->
+    <!-- END Custom CSS-->
   </head>
-  <body>
-  <center>
-    <p><a href="searchmodules.php">Search Modules</a></p>
-    <p><a href="viewstudentmodules.php">My Modules</a></p>
-    <p><a href="">Chat</a></p>
-    <p><a href="">My Profile</a></p>
-    <p><a href="complain.php">Feedback</a></p>
-    <p><a href="viewtimetable.php">My Timetable</a></p>
-    <p><a href="logout.php">Logout</a></p>
-
-	<br/>
-
-
+<body class="vertical-layout vertical-menu 2-columns menu-expanded fixed-navbar" data-open="click" data-menu="vertical-menu" data-color="bg-gradient-x-purple-blue" data-col="2-columns">
 
 <?php
 session_start();
 include 'config.php';
-echo "Logged in As:<br/>";
-$user_id=$_SESSION['user_id'];
-$username=$_SESSION['username'];
-echo "$username<br/>";
-$login_user=$_SESSION['login_user'];
-$sql = "SELECT * FROM notification WHERE receiver = '$username' AND isRead = '0';";
-$result = mysqli_query($db, $sql);
-if (mysqli_num_rows($result) > 0){
-  $sql1 = "SELECT COUNT(*) FROM notification WHERE receiver='$username' and isRead = '0' GROUP BY receiver;";
-  $result1 = mysqli_query($db,$sql1);
-  $row1 = mysqli_fetch_row($result1);
-  echo "Notifications(".$row1[0].")";
-  while ($row = mysqli_fetch_row($result)){
-    echo "<h5>" . $row[1] . "</h5>";
-    $tempstore[] = $row[0];
-  }
-  foreach ($tempstore as $value) {
-    $sql2 = "UPDATE notification SET isRead = '1' WHERE id = '$value' AND receiver = '$username'";
-    $result2 = mysqli_query($db, $sql2);
-    if (!$result2){
-      echo "unsuccessful";
-    }
-  }
-}
-else{
-  echo "Notifications";
-}
+include './layout/sidebar.php';
+
+?> 
+  <div class="app-content content">
+      <div class="content-wrapper">
+        <div class="content-wrapper-before"></div>
+        <div class="content-header row">
+          <div class="content-header-left col-md-4 col-12 mb-2">
+            <h3 class="content-header-title">Dashboard</h3>
+          </div>
+          
+        </div>
+
+        <div class="content-body">
+          
+            <div class="row">
+              <div class="col-12">
+                <div class="card">
+                  <div class="card-header">
+                    <h4 class="card-title">Title</h4>
+                    <div class="card-content">
+                    <div class="card-body">
+                            <div class="height-400">
+                          hello!
+                        </div>
+                    </div>
+                </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div class="row">
+              <div class="col-12">
+                <div class="card">
+                  <div class="card-header">
+                    <h4 class="card-title">Another card</h4>
+                    <div class="card-content">
+                    <div class="card-body">
+                            <div class="height-400">
+                          hello!
+                        </div>
+                    </div>
+                </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+          </div>
+          
+        </div>  <!-- end of content-body -->
+      </div>  <!-- end of content-wrapper -->
+    </div>  <!-- end of app-content content -->
+
 ?>
 
 
+  <!-- BEGIN VENDOR JS-->
+    <script src="./layout/theme-assets/vendors/js/vendors.min.js" type="text/javascript"></script>
+    <!-- BEGIN VENDOR JS-->
+    <!-- BEGIN PAGE VENDOR JS-->
+    <script src="./layout/theme-assets/vendors/js/charts/chartist.min.js" type="text/javascript"></script>
+    <!-- END PAGE VENDOR JS-->
+    <!-- BEGIN CHAMELEON  JS-->
+    <script src="./layout/theme-assets/js/core/app-menu-lite.js" type="text/javascript"></script>
+    <script src="./layout/theme-assets/js/core/app-lite.js" type="text/javascript"></script>
+    <!-- END CHAMELEON  JS-->
+    <!-- BEGIN PAGE LEVEL JS-->
+    <script src="./layout/theme-assets/js/scripts/pages/dashboard-lite.js" type="text/javascript"></script>
+    <!-- END PAGE LEVEL JS-->
 
-
-	</center>
   </body>
 </html>
