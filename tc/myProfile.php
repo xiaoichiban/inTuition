c12959208cda8368dae8f10fc287991847edda65
apollo717 <54750246+apollo717@users.noreply.github.com>
2019-11-09 12:56:52 +0800

    Create myProfile.php
    for tc

---
+++ tc/myProfile.php
@@ -0,0 +1,28 @@
+<?php
+
+include('layout.php');
+
+
+
+
+// Get Session Username
+$thisusername = $_SESSION['username'];
+
+
+// echo $thisusername;
+
+
+
+
+
+// SQL PART
+// $sqlSTRING = "SELECT * FROM tc WHERE username = '$thisusername' "
+
+
+
+
+
+
+
+
+?>
