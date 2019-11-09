bf4c5e9cf07fd2e7b6c6f00c7726272dcf5f5d20
DESKTOP-T5T7LS5\Zhi Kuan <xiaoichiban@hotmail.com>
2019-11-09 20:43:27 +0800

    student edit profile process

--- student/editProfilePictureProcess.php
+++ student/editProfilePictureProcess.php
@@ -1,28 +1,25 @@
 <?php
 
-include('layout.php');
+// include('layout.php');
 
-$target_dir = "./profile/";
+
+include('config.php');
+
+session_start();
+
+$target_dir = "../profilepics/";
 
 $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
 $uploadOk = 1;
 $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
 
 
-
-echo basename($_FILES["fileToUpload"]["name"]);
-
-
-
-
-
- 
 // Check if image file is a actual image or fake image
 if(isset($_POST["submit"])) {
     $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
-	
 
-	
+
+
     if($check !== false) {
         // echo "File is an image - " . $check["mime"] . ".";
         $uploadOk = 1;
@@ -32,17 +29,6 @@ if(isset($_POST["submit"])) {
     }
 }
 
-
-/*
-// Check if file already exists
-if (file_exists($target_file)) {
-    echo "Sorry, file already exists. Error";
-    $uploadOk = 0;
-}
-*/
-
-
-	
 
 
 
@@ -61,55 +47,55 @@ if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg
 if ($uploadOk == 0) {
     echo "Sorry, your file was not uploaded.";
 // if everything is ok, try to upload file
-} 
+}
 else {
-	
-	
+
+
 	$extension = explode(".", $_FILES["fileToUpload"]["name"]);
-	$newfilename = "./profile/".round(microtime(true)) . '.' . end($extension);
+	$newfilename = "../profilepics/".round(microtime(true)) . '.' . end($extension);
 	$smallfilename = round(microtime(true)) . '.' . end($extension);
-	
-	
-	
+
+
+
     if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $newfilename)) {
         // echo "The file ".$newfilename. " has been uploaded.";
         echo "<div align='center'>";
 
-        echo "<h3 align='center'> GOOD </h3>";
-		
+        echo "<h4 align='center'> GOOD </h4>";
+
 		$thisusername = $_SESSION['username'];
-		
-		$sql = "UPDATE account SET profilepic = '$smallfilename' WHERE username ='$thisusername';";
+
+		$sql = "UPDATE account SET avatar_path = '$smallfilename' WHERE username ='$thisusername';";
 
 		if ($db->query($sql) === TRUE) {
 			echo "Update successfully";
 		} else {
 			echo "Error: !!  <br>";
 		}
-		
+
 		echo "</div>";
-		
-		
-    } 
+
+
+    }
 	else {
         echo "<h3 align='center'>Sorry, there was an error uploading your picture file. Please try again. </h3>";
     }
-	
+
 
 	echo "<br/><br/>
 	<div align='center'>
-	<h3>You will be redirected in <div id='counter'>5</h3>
+	<h3>Ready in <div id='counter'>5</h3>
 	<script>
 	setInterval(function() {
             var div = document.querySelector('#counter');
             var count = div.textContent * 1 - 1;
             div.textContent = count;
-            if (count <= 0) {window.location.replace('./myprofile.php');  }
+            if (count <= 0) {window.location.replace('./viewProfile.php');  }
         }, 1000);
     </script>
-	<img src='./load.gif'  />
+	<img src='../tutor/loader.gif' class='img-fluid' alt='Responsive image' />
 	<br/>
-	<a href = './myprofile.php'>Alternatively you can click here to redirect !</a>
+	<a href = './viewProfile.php'>Alternatively you can click here to redirect !</a>
 	</div>
 	";
 
@@ -117,10 +103,3 @@ else {
 
 
 ?>
-
-
-
-
-
-
-
