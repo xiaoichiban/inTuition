cdfd6ec488887a8ee5fdbee81062464204114b60
DESKTOP-T5T7LS5\Zhi Kuan <xiaoichiban@hotmail.com>
2019-11-09 19:20:28 +0800

    add Tutor profile page and edit profile picture function

--- tutor/editProfilePicture.php
+++ tutor/editProfilePicture.php
@@ -1,34 +1,37 @@
 <?php
-include('layout.php');
+//include('layout.php');
+
+session_start();
+
 ?>
 
 
 
-<body>  
+<body>
 	<div align="center">
 		<h3>Edit Profile Picture</h3>
-		<br/>	
-<?php 
+		<br/>
+<?php
 
   $dba = mysqli_connect("localhost", "admin", "admin", "petdb");
-  
+
   if (!$dba) {
     die("Connection failed: " . mysqli_connect_error());
 	}
-					
+
 /* Attempt MySQL server connection. Assuming you are running MySQL
 server with default setting (user 'root' with no password) */
-  
+
 // Attempt select query execution
 $targetUsername = $_SESSION['username'];
 $sql = "SELECT * FROM account WHERE username = '$targetUsername' ";
 if($result = mysqli_query($dba, $sql)){
-    
+
 	if(mysqli_num_rows($result) > 0){
-	
+
 	$row = mysqli_fetch_array($result);
-	
-		
+
+
 echo "
 
 
@@ -41,56 +44,56 @@ echo "
     <div class='col'>
       <br/>
     </div>
-	
-	
+
+
     <div class='col'>
 		<form action='editProfilePictureProcess.php' method='post' enctype='multipart/form-data'>
-		
-			<b>profilepic:</b> <br/> <img src='./profile/" . $row['avatar_path'] . "' width='200px' height='200px' />
+
+			<b>profilepic:</b> <br/> <img src='../profilepics/" . $row['avatar_path'] . "' width='200px' height='200px' />
 			<br/><br/>
 
 			<script src='./js/jslib.js'></script>
-	
+
 			<label>Select image to upload:</label>
-			
+
 			<label class='file-upload btn btn-primary'>
 
-			<input type='file' class='form-control-file'  
+			<input type='file' class='form-control-file'
 				accept='image/*' name='fileToUpload' id='fileToUpload' required='true' onchange='checkFileSize(this)'>
-			</label>		
-			
+			</label>
+
 			<br/> <br/>
 
 			<button type='submit' class='btn btn-success'>Change Profile Picture</button>
-	  
+
 		</form>
     </div>
-	
-	
+
+
     <div class='col'>
       <br/>
     </div>
-	
+
   </div>
 ";
 
         // Free result set
         mysqli_free_result($result);
-		
-    } 
-	
+
+    }
+
 	else{
         echo "No records matching your query were found.";
     }
-} 
+}
 else{
     echo "ERROR: Could not able to execute $sql. " . mysqli_error($dba);
 }
- 
+
 // Close connection
 mysqli_close($dba);
 
 
 ?>
-		
-</body>  
+
+</body>
