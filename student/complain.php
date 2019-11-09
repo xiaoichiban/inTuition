211e4fb4672768b1433f93a181e73d6637045b31
RouJie <e0196675@u.nus.edu>
2019-11-09 19:54:27 +0800

    improve feedback ui

--- student/complain.php
+++ student/complain.php
@@ -1,216 +1,110 @@
-
-<?php
-include 'config.php';
-include 'session.php';
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $username = $_SESSION['username'];
-    $title = $_POST['title'];
-    $problem = $_POST['problem'];
-    $sql = "INSERT INTO complain(title, content, complainer, status)"
-            . "VALUES('$title', '$problem', '$username', 'new');";
-    $results = mysqli_query($db, $sql);
-    if ($results) {
-        echo "<script type = 'text/javascript'> alert ('Thank you for your report!!')</script>";
-    } else {
-        echo "<script type = 'text/javascript'> alert ('Error in Submission!!')</script>";
-    }
-    echo '<script>window.location.href = "studentdashboard.php";</script>';
-    exit();
-}
-?>
 <html>
-    <head>
-        <title>Submit Feedback</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <style>
-            * {
-                box-sizing: border-box;
-            }
-            @media screen and (max-width:600px) {
-                .column {
-                    width: 100%;
-                }
-            }
-            body {
-                font-family: Old Standard;
-                color: black;
-                margin: 0;
-            }
+<head>
+  <title>Feedback</title>
 
-            /* Style the header */
-            .header {
-                background-color: #f1f1f1;
-                padding: 5px;
-                text-align: center;
-            }
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="layout/timetablestyle.css">
+  <link rel="apple-touch-icon" href="./layout/theme-assets/images/ico/apple-icon-120.png">
+  <link rel="shortcut icon" type="image/x-icon" href="./layout/theme-assets/images/ico/favicon.ico">
+  <link href="https://fonts.googleapis.com/css?family=Muli:300,300i,400,400i,600,600i,700,700i%7CComfortaa:300,400,700" rel="stylesheet">
+  <link href="https://maxcdn.icons8.com/fonts/line-awesome/1.1/css/line-awesome.min.css" rel="stylesheet">
+  <!-- BEGIN VENDOR CSS-->
+  <link rel="stylesheet" type="text/css" href="./layout/theme-assets/css/vendors.css">
+  <link rel="stylesheet" type="text/css" href="./layout/theme-assets/vendors/css/charts/chartist.css">
+  <!-- END VENDOR CSS-->
+  <!-- BEGIN CHAMELEON  CSS-->
+  <link rel="stylesheet" type="text/css" href="./layout/theme-assets/css/app-lite.css">
+  <!-- END CHAMELEON  CSS-->
+  <!-- BEGIN Page Level CSS-->
+  <link rel="stylesheet" type="text/css" href="./layout/theme-assets/css/core/menu/menu-types/vertical-menu.css">
+  <link rel="stylesheet" type="text/css" href="./layout/theme-assets/css/core/colors/palette-gradient.css">
+  <link rel="stylesheet" type="text/css" href="./layout/theme-assets/css/pages/dashboard-ecommerce.css">
+  <!-- END Page Level CSS-->
+  <!-- BEGIN Custom CSS-->
+  <!-- END Custom CSS-->
+</head>
+<body class="vertical-layout vertical-menu 2-columns menu-expanded fixed-navbar" data-open="click" data-menu="vertical-menu" data-color="bg-gradient-x-purple-blue" data-col="2-columns">
 
-            .column {
-                float: left;
-                width: 33.33%;
-                padding: 15px;
-            }
-            .row:after {
-                content: "";
-                display: table;
-                clear: both;
-            }
+  <?php
+  include 'session.php';
+  include 'config.php';
+  include './layout/sidebar.php';
 
-            .column {
-                float: left;
-                width: 25%;
-                padding: 15px;
-            }
+  ?>
+  <div class="app-content content">
+    <div class="content-wrapper">
+      <div class="content-wrapper-before"></div>
+      <div class="content-header row">
+        <div class="content-header-left col-md-4 col-12 mb-2">
+          <h3 class="content-header-title">Feedback</h3>
+        </div>
 
-            .columnright {
-                float: left;
-                width: 75%;
-                padding: 15px;
-            }
-            .row:after {
-                content: "";
-                display: table;
-                clear: both;
-            }
+      </div>
 
-            /* Style the top navigation bar */
-            .topnav {
-                overflow: hidden;
-                background-color: #333;
-                text-align: center;
-                display: flex;
-                justify-content: space-around;
-                padding-left: 150px;
-                padding-right: 150px;
-            }
+      <div class="content-body">
 
-            /* Style the topnav links */
-            .topnav a {
-                float: left;
-                display: block;
-                color: #f2f2f2;
-                text-align: center;
-                padding: 14px 16px;
-                text-decoration: none;
-                width:150px;
-            }
+        <div class="row">
+          <div class="col-12">
+            <div class="card">
+              <div class="card-header">
+                <h4 class="card-title">Feedback Form</h4>
+                <div class="card-content">
+                  <div class="card-body">
+                    <form action="complain.php" method="post" enctype="multipart/form-data">
+                        <div class='basic-inputs'>
+                            <br/>
+                            <fieldset class="form-group">
+                                <h5 align='center' class="mt-2">Title: &nbsp;</h5>
+                                <input type="text" name="title" id="title" class="form-control" id="placeholderInput" placeholder="Enter Title..." required>
+                            </fieldset>
+                            <fieldset class="form-group">
+                                <h5 align='center' class="mt-2">Feedback: &nbsp;</h5>
+                                <input type="text" name="problem" id="problem" class="form-control" id="placeholderInput" placeholder="Enter Feedback..." required>
+                            </fieldset>
 
-            /* Change color on hover */
-            .topnav a:hover {
-                background-color: #ddd;
-                color: black;
-            }
+                            <br/>
+                        </div>
+                        <div align='center'>
+                            <br/>
+                            <input type="submit" value="Submit" name="submit" class="btn btn-primary btn-min-width mr-1 mb-1">
+                        </div>
+                    </form>
 
-            .dropdown {
-                float: left;
-                overflow: hidden;
-            }
 
-            .dropdown .dropbtn {
-                font-size: 16px;
-                border: none;
-                outline: none;
-                color: white;
-                padding: 14px 16px;
-                background-color: inherit;
-                font-family: inherit;
-                margin: 0;
-                width:150px;
-            }
-
-            .navbar a:hover, .dropdown:hover .dropbtn {
-                background-color: bisque;
-                color: black;
-            }
-
-            .dropdown-content {
-                display: none;
-                position: absolute;
-                background-color: #f9f9f9;
-                min-width: 160px;
-                box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
-                z-index: 1;
-            }
-
-            .dropdown-content a {
-                float: none;
-                color: black;
-                padding: 12px 16px;
-                text-decoration: none;
-                display: block;
-                text-align: left;
-            }
-
-            .dropdown-content a:hover {
-                background-color: #ddd;
-            }
-
-            .dropdown:hover .dropdown-content {
-                display: block;
-            }
-
-            .imagecontainer {
-                position:absolute;
-                bottom:0;
-            }
-
-            .report {
-                align:center;
-                text-align: center;
-                background-color:#D7DBDD;
-                border-radius: 4px;
-                max-width: 80%;
-                margin: auto;
-            }
-
-            input[type=text] {
-                width: 50%;
-                padding: 12px 20px;
-                margin: 8px 0;
-                display: inline-block;
-                border: 1px solid #ccc;
-                border-radius: 4px;
-                box-sizing: border-box;
-            }
-
-            input[type=text]:focus {
-                border: 3px solid #555;
-            }
-
-            input[type=submit] {
-                border-radius: 4px;
-            }
-
-            input[type=submit]:hover {
-                background-color: white;
-                color: black;
-                cursor: pointer;
-
-            }
-        </style>
-    </head>
-    <body>
-
-        <form action="complain.php" method="post" enctype="multipart/form-data">
-            <div class='report'>
-                <br/>
-                <h3 align='center'>Title: &nbsp;<input type="text" name="title" id="title"></h3>
-                <h3 align='center'>Problem: &nbsp;<input type="text" name="problem" id="problem"></h3>
-                <br/>
+                    <h6><a href = 'studentdashboard.php'>Back</a></h6>
+                  </div>
+                </div>
+              </div>
             </div>
-            <div align='center'>
-                <br/>
-                <input type="submit" value="Submit" name="submit" style='font-family:Old Standard;font-size: 18px'>
-            </div>
-        </form>
+          </div>
+        </div>
+
+      </div>  <!-- end of content-body -->
+    </div>  <!-- end of content-wrapper -->
+  </div>  <!-- end of app-content content -->
+
+  ?>
 
 
-        <div align='center'><h3><a href = 'studentdashboard.php' style='font-family:Old Standard;font-size: 18px'>Back</a></h3></div>
-    </body>
+  <!-- BEGIN VENDOR JS-->
+  <script src="./layout/theme-assets/vendors/js/vendors.min.js" type="text/javascript"></script>
+  <!-- BEGIN VENDOR JS-->
+  <!-- BEGIN PAGE VENDOR JS-->
+  <script src="./layout/theme-assets/vendors/js/charts/chartist.min.js" type="text/javascript"></script>
+  <!-- END PAGE VENDOR JS-->
+  <!-- BEGIN CHAMELEON  JS-->
+  <script src="./layout/theme-assets/js/core/app-menu-lite.js" type="text/javascript"></script>
+  <script src="./layout/theme-assets/js/core/app-lite.js" type="text/javascript"></script>
+  <!-- END CHAMELEON  JS-->
+  <!-- BEGIN PAGE LEVEL JS-->
+  <script src="./layout/theme-assets/js/scripts/pages/dashboard-lite.js" type="text/javascript"></script>
+  <!-- END PAGE LEVEL JS-->
+
+</body>
 </html>
 
 <?php
-include 'config.php';
-include 'session.php';
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $username = $_SESSION['username'];
     $title = $_POST['title'];
