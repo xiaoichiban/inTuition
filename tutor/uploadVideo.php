f7b30e60d2b75bec418a1b60613a60b5870c5dc9
yinggs <yinghuiseah@u.nus.edu>
2019-11-12 17:25:53 +0800

    rectified tutor view module video list and upload video

--- tutor/uploadVideo.php
+++ tutor/uploadVideo.php
@@ -33,97 +33,79 @@ include './layout/sidebar.php';
 
 ?>
 
-
-
-
 <div class="app-content content">
     <div class="content-wrapper">
       <div class="content-wrapper-before"></div>
       <div class="content-header">
         <div class="content-header-left col-md-4 col-12 mb-2">
-          <h3 class="content-header-title"> Upload Videos</h3>
+          <h3 class="content-header-title">Upload Videos</h3>
         </div>
 
         <div class="content-body">
 
           <div class="row">
+            <div class="col-12">
+              <div class="card">
+                <div class="card-content">
+                  <div class="card-body">
+
+                      <form action="uploadVideoProcess.php" method="post" enctype="multipart/form-data">
+                          
+                        <label for="vidname">Video name</label>
+                        <br/>
+                        <input type="text" style="width: 40%;" id="vidname" name="vidname" maxlength="20" class="form-control" required> </input>
+
+                        <br/>
+                        <br/>
+                        
+                        <label for="videscript">Video Description</label>
+                        <br/>
+                        <textarea style="width: 40%;" id="videscript" name="videscript" maxlength="50" class="form-control" required> </textarea>
+
+                        <br/>
+                        <br/>
+                        
+                        <label for="videscript">Module Code</label>
+                        <br/>
+                        <input type="text" style="width: 40%;" id="modcode" name="modcode" 
+                        maxlength="5" class="form-control" required> </input>
+
+                        <br/>
+                        <br/>
+                          
+                        <div class="form-group">
+                        <script src="./js/jslib.js"></script>
+                        <label class="file-upload">
+                          Select Video to upload: <br/>
+                          <input type="file" class="form-control-file"  
+                            accept='video/mp4' name="fileToUpload" id="fileToUpload" 
+                            onchange="checkFileSize(this)">
+                        </label>  
+                        </div>  
+                        
+                        
+                        <div class="form-group">
+                          Select Subtitles to upload (optional): <br/>
+                          <input type="file" class="form-control-file"  
+                            accept='.vtt' name="subsToUpload" id="subsToUpload" >
+                        </div>  
+                            
+                        <br/>
+                        
+                        <button type="submit" name="submit" class="btn btn-primary">Submit</button>                        
+                        
+                        <br/>
+                        <br/>
+                        
+                      </form>
 
 
+                  </div>
+                </div>
+              </div>
+            </div> <!-- row --> 
 
-
-
-
-
-<center>
-
-<form action="uploadVideoProcess.php" method="post" enctype="multipart/form-data">
-    
-	<label for="vidname">Video name</label>
-	<br/>
-	<input type="text"  id="vidname" name="vidname" maxlength="20" class="form-control" required> </input>
-
-	<br/>
-	<br/>
-	
-	<label for="videscript">Video Description</label>
-	<br/>
-	<input type="text"  id="videscript" name="videscript" maxlength="50" class="form-control" required> </input>
-
-	<br/>
-	<br/>
-	
-	<label for="videscript">Module Code</label>
-	<br/>
-	<input type="text"  id="modcode" name="modcode" 
-	maxlength="5" class="form-control" required> </input>
-
-	<br/>
-	<br/>
-
-	
-  	
-	<div class="form-group">
-	<script src="./js/jslib.js"></script>
-	<label class="file-upload btn btn-primary">
-		Select Video to upload: <br/>
-		<input type="file" class="form-control-file"  
-			accept='video/mp4' name="fileToUpload" id="fileToUpload" 
-			onchange="checkFileSize(this)">
-	</label>	
-	</div>	
-			
-	<br/>
-	<br/>
-
-	
-	
-	
-	<div class="form-group">
-		Select Subtitles to upload (optional) : <br/>
-		<input type="file" class="form-control-file"  
-			accept='.vtt' name="subsToUpload" id="subsToUpload" >
-	</div>	
-			
-	<br/>
-
-	
-	
-	<div align='center'>
-	<button type="submit" name="submit" class="btn btn-success">Submit</button>
-	</div>
-	
-	
-	<br/>
-	<br/>
-	
-</form>
-
-
-
-
-</center>
-
-
+          
     </div>
 
 	</div>
