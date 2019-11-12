0a8e2ee8996a47d37b88f676ec282fbc78bfd032
DESKTOP-T5T7LS5\Zhi Kuan <xiaoichiban@hotmail.com>
2019-11-12 22:02:44 +0800

    fix TC sidebar

--- tc/layout/sidebar.php
+++ tc/layout/sidebar.php
@@ -25,7 +25,7 @@
 <div class="main-menu menu-fixed menu-light menu-accordion menu-shadow" data-scroll-to-active="true" data-img="../theme-assets/images/backgrounds/02.jpg">
       <div class="navbar-header">
         <ul class="nav navbar-nav flex-row">
-          <li class="nav-item mr-auto"><a class="navbar-brand" href="studentdashboard.php"><img class="brand-logo" alt="Chameleon admin logo" src="./layout/theme-assets/images/logo/intuition_logo.png"/>
+          <li class="nav-item mr-auto"><a class="navbar-brand" href="tcdashboard.php"><img class="brand-logo" alt="Chameleon admin logo" src="./layout/theme-assets/images/logo/intuition_logo.png"/>
               <h3 class="brand-text">inTuition</h3></a></li>
           <li class="nav-item d-md-none"><a class="nav-link close-navbar"><i class="ft-x"></i></a></li>
         </ul>
