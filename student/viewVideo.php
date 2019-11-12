5d124670d37c4cd32f20cbb30fdb713791749d1a
DESKTOP-T5T7LS5\Zhi Kuan <xiaoichiban@hotmail.com>
2019-11-12 22:04:33 +0800

    add title and description to viewVideo page

--- student/viewVideo.php
+++ student/viewVideo.php
@@ -1,66 +1,123 @@
-<?php
+<html>
+<head>
+	<title>Dashboard</title>
 
-$id = null;
-$subs = "blank.vtt";
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="layout/timetablestyle.css">
+	<link rel="apple-touch-icon" href="./layout/theme-assets/images/ico/apple-icon-120.png">
+	<link rel="shortcut icon" type="image/x-icon" href="./layout/theme-assets/images/ico/favicon.ico">
+	<link href="https://fonts.googleapis.com/css?family=Muli:300,300i,400,400i,600,600i,700,700i%7CComfortaa:300,400,700" rel="stylesheet">
+	<link href="https://maxcdn.icons8.com/fonts/line-awesome/1.1/css/line-awesome.min.css" rel="stylesheet">
+	<!-- BEGIN VENDOR CSS-->
+	<link rel="stylesheet" type="text/css" href="./layout/theme-assets/css/vendors.css">
+	<link rel="stylesheet" type="text/css" href="./layout/theme-assets/vendors/css/charts/chartist.css">
+	<!-- END VENDOR CSS-->
+	<!-- BEGIN CHAMELEON  CSS-->
+	<link rel="stylesheet" type="text/css" href="./layout/theme-assets/css/app-lite.css">
+	<!-- END CHAMELEON  CSS-->
+	<!-- BEGIN Page Level CSS-->
+	<link rel="stylesheet" type="text/css" href="./layout/theme-assets/css/core/menu/menu-types/vertical-menu.css">
+	<link rel="stylesheet" type="text/css" href="./layout/theme-assets/css/core/colors/palette-gradient.css">
+	<link rel="stylesheet" type="text/css" href="./layout/theme-assets/css/pages/dashboard-ecommerce.css">
+	<!-- END Page Level CSS-->
+	<!-- BEGIN Custom CSS-->
+	<!-- END Custom CSS-->
+</head>
+<body class="vertical-layout vertical-menu 2-columns menu-expanded fixed-navbar" data-open="click" data-menu="vertical-menu" data-color="bg-gradient-x-blue-cyan" data-col="2-columns">
 
-// print_r($_GET);
-if(isset($_GET["id"])) {
-	$id = $_GET["id"];
-	// echo "$id";
-}
+	<?php
+	include('session.php');
+
+	include './layout/sidebar.php';
+
+	$id = null;
+	$subs = "blank.vtt";
+
+	// print_r($_GET);
+	if(isset($_GET["id"])) {
+		$id = $_GET["id"];
+		// echo "$id";
+	}
 
 
-if(isset($_GET["subs"])) {
-	$subs = $_GET["subs"];
-	
-	if ($subs==''){
+	if(isset($_GET["subs"])) {
+		$subs = $_GET["subs"];
+
+		if ($subs==''){
+			$subs = "blank.vtt";
+		}
+		// echo "$subs";
+	}
+	else{
 		$subs = "blank.vtt";
 	}
-	// echo "$subs";
-}
-else{
-	$subs = "blank.vtt";
-}
 
 
-if($id == null || $id =='' ){
-	return;
-}
+	if($id == null || $id =='' ){
+		return;
+	}
 
+	$sqlQuery = "SELECT * from video v  WHERE v.filename = '$id';";
 
+	$result = mysqli_query($db, $sqlQuery);
+	$row = mysqli_fetch_assoc($result);
+	//poster='loads3.gif'
+	?>
 
+	<div class="app-content content">
+		<div class="content-wrapper">
+			<div class="content-wrapper-before"></div>
+			<div class="content-header row">
+				<div class="content-header-left col-md-4 col-12 mb-2">
+					<h3 class="content-header-title">Video</h3>
+				</div>
+			</div>
 
+			<div class="content-body">
+				<div class="row">
+					<div class="col-12">
+						<div class="card">
 
+							<div class="card-content">
+								<div class="card-body">
 
-//poster='loads3.gif'
+									<?php
 
+									$name = $row['name'];
+									$description = $row['description'];
 
-echo "
-<center>
-<br/>
-<a href= 'studentdashboard.php' > << Go Back Home<< </a>
-<br/>
-<br/>
-<button onclick='goBack()'>Go Back</button> <script> function goBack() { window.history.back(); } </script>
-<br/>
-<br/>
+									echo "
+									<center>
+									<h3><b>Title: $name</b></h3><br>
+									<h5>$description</h5>
+									<video
+									id='myVideo'
+									name='myVideo'
+									controls
+									style='width:640px;height:360px;'
+									>
 
-<video 
-id='myVideo' 
-name='myVideo'
-controls
-style='width:640px;height:360px;' 
- >
+									<source src='../tutor/video/$id'  />
+									<track src='../tutor/video/$subs' label='English subtitles'
+									kind='subtitles' srclang='en' default></track>
+									</video>
 
-  <source src='../tutor/video/$id'  />
-  <track src='../tutor/video/$subs' label='English subtitles' 
-         kind='subtitles' srclang='en' default></track>
-</video>
+									</center>
+									";
+									?>
+								</div>
+							</div>
+						</div> <!-- end of card -->
+					</div> <!-- end of col-12 -->
+				</div> <!-- end of row -->
+			</div> <!-- end of content body -->
 
-</center>
-";
-?>
+			<button  class='btn btn-primary' onclick='goBack()'>Go Back</button>
+			<script>function goBack() {window.history.back();}</script>
 
+		</div> <!-- content wrapper -->
+	</div> <!-- app content -->
 
-
-
+</body>
+</html>
--- tutor/viewVideo.php
+++ tutor/viewVideo.php
@@ -26,48 +26,51 @@
 </head>
 <body class="vertical-layout vertical-menu 2-columns menu-expanded fixed-navbar" data-open="click" data-menu="vertical-menu" data-color="bg-gradient-x-blue-cyan" data-col="2-columns">
 
-<?php
-include('session.php');
+  <?php
+  include('session.php');
 
-include './layout/sidebar.php';
+  include './layout/sidebar.php';
 
-$id = null;
-$subs = "blank.vtt";
+  $id = null;
+  $subs = "blank.vtt";
 
-// print_r($_GET);
-if(isset($_GET["id"])) {
-	$id = $_GET["id"];
-	// echo "$id";
+  // print_r($_GET);
+  if(isset($_GET["id"])) {
+    $id = $_GET["id"];
+    // echo "$id";
 
-  $sql = "SELECT mod_id FROM video WHERE filename = '$id'; ";
-  $result = mysqli_query($db, $sql);
-  $row = mysqli_fetch_row($result);
-}
-
-
-if(isset($_GET["subs"])) {
-	$subs = $_GET["subs"];
-	
-	if ($subs==''){
-		$subs = "blank.vtt";
-	}
-	// echo "$subs";
-}
-else{
-	$subs = "blank.vtt";
-}
+    $sql = "SELECT mod_id FROM video WHERE filename = '$id'; ";
+    $result = mysqli_query($db, $sql);
+    $row = mysqli_fetch_row($result);
+  }
 
 
-if($id == null || $id =='' ){
-	return;
-}
+  if(isset($_GET["subs"])) {
+    $subs = $_GET["subs"];
+
+    if ($subs==''){
+      $subs = "blank.vtt";
+    }
+    // echo "$subs";
+  }
+  else{
+    $subs = "blank.vtt";
+  }
 
 
-//poster='loads3.gif'
+  if($id == null || $id =='' ){
+    return;
+  }
 
-?>
+  $sqlQuery = "SELECT * from video v  WHERE v.filename = '$id';";
 
-<div class="app-content content">
+	$result = mysqli_query($db, $sqlQuery);
+	$row = mysqli_fetch_assoc($result);
+
+  //poster='loads3.gif'
+  ?>
+
+  <div class="app-content content">
     <div class="content-wrapper">
       <div class="content-wrapper-before"></div>
       <div class="content-header row">
@@ -80,41 +83,46 @@ if($id == null || $id =='' ){
         <div class="row">
           <div class="col-12">
             <div class="card">
-                
+
               <div class="card-content">
                 <div class="card-body">
-                <?
+
+                  <?php
+
+                  $name = $row['name'];
+									$description = $row['description'];
+
                   echo "
                   <center>
-
-                  <video 
-                  id='myVideo' 
+                  <h3><b>Title: $name</b></h3><br>
+									<h5>$description</h5>
+                  <video
+                  id='myVideo'
                   name='myVideo'
                   controls
-                  style='width:640px;height:360px;' 
-                   >
+                  style='width:640px;height:360px;'
+                  >
 
-                    <source src='video/$id'  />
-                    <track src='video/$subs' label='English subtitles' 
-                           kind='subtitles' srclang='en' default></track>
+                  <source src='./video/$id'  />
+                  <track src='./video/$subs' label='English subtitles'
+                  kind='subtitles' srclang='en' default></track>
                   </video>
 
                   </center>
-                  <br><br>
-                  ";
-                ?>
+                  <br><br> ";
+                  ?>
                 </div>
               </div>
-            </div> <!-- end of card --> 
-          </div> <!-- end of col-12 --> 
-        </div> <!-- end of row --> 
+            </div> <!-- end of card -->
+          </div> <!-- end of col-12 -->
+        </div> <!-- end of row -->
       </div> <!-- end of content body -->
 
-    <button class='btn btn-primary'><a style="color:white;" href = 'viewmodule.php?module_id=<?= $row[0] ?>'>Back</a></button>
+      <button  class='btn btn-primary' onclick='goBack()'>Go Back</button>
+      <script>function goBack() {window.history.back();}</script>
 
-  </div> <!-- content wrapper --> 
-</div> <!-- app content --> 
+    </div> <!-- content wrapper -->
+  </div> <!-- app content -->
 
 </body>
 </html>
-
