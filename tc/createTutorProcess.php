4dcff792892be116682503e291feb03cb9983038
DESKTOP-T5T7LS5\Zhi Kuan <xiaoichiban@hotmail.com>
2019-11-13 13:46:39 +0800

    fix create Tutor process issue

--- tc/createTutorProcess.php
+++ tc/createTutorProcess.php
@@ -6,22 +6,22 @@
   $tc = $_SESSION['login_user'];
   $username = mysqli_real_escape_string($db, $_POST['username']);
   $password = mysqli_real_escape_string($db, $_POST['password']);
-  
-  
+
+
   $password = password_hash($password, PASSWORD_DEFAULT);
-  
-  
+
+
   $name = mysqli_real_escape_string($db, $_POST['name']).strtolower();
   $email = mysqli_real_escape_string($db, $_POST['email']).strtolower();
   $status = 'active';
   // $tc_owner = mysqli_real_escape_string($db, $_POST['tc']);
 
-  $sql = "INSERT INTO account (username, password, name, email, status, date_registered) 
+  $sql = "INSERT INTO account (username, password, name, email, status, date_registered)
   VALUES ('$username', '$password', '$name', '$email', '$status' , now())";
 
   if ($db->query($sql) === TRUE) {
       echo $username . " account created successfully!";
-      $sql = "INSERT INTO tutor (username, tc_owner) VALUES ('$name', '$tc')";
+      $sql = "INSERT INTO tutor (username, tc_owner) VALUES ('$username', '$tc')";
       if ($db->query($sql) === TRUE) {
         echo $username . " tutor created successfully!";
       }
